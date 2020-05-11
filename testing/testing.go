26d346087316d602ff3b0c3329798a0e70f31709
Paddy Carver <paddy@impractical.co>
2020-05-10 18:27:25 -0700

    Add testing sync.
    Add a sync that uses the testing.T's Log method, to properly log during
    test runs.

---
+++ testing/testing.go
@@ -0,0 +1,74 @@
+package testing
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	yall "yall.in"
+)
+
+type Logger struct {
+	t   *testing.T
+	sev yall.Severity
+}
+
+func (l Logger) shouldLog(sev yall.Severity) bool {
+	switch {
+	case l.sev == yall.Error && sev != yall.Error:
+		return false
+	case l.sev == yall.Warning && sev != yall.Error && sev != yall.Warning:
+		return false
+	case l.sev == yall.Info && sev != yall.Error && sev != yall.Warning && sev != yall.Info:
+		return false
+	default:
+		// when logging at debug level log default & unknown sevs, too
+		return true
+	}
+}
+
+func New(t *testing.T, sev yall.Severity) Logger {
+	return Logger{
+		t:   t,
+		sev: sev,
+	}
+}
+
+type sprintf func(string, ...interface{}) string
+
+func (l Logger) AddEntry(e yall.Entry) {
+	l.t.Helper()
+	if l.t == nil {
+		return
+	}
+	if !l.shouldLog(e.Severity) {
+		return
+	}
+	var msg string
+	if v, ok := e.Payload["msg"]; ok {
+		msg = fmt.Sprintf(" %v", v)
+	}
+
+	var fields []string
+	for k, v := range e.Payload {
+		if k == "msg" {
+			continue
+		}
+		fields = append(fields, fmt.Sprintf("%s=%v", k, v))
+	}
+
+	l.t.Logf("%s [%s]%s\n", e.LoggedAt.Local().Format("2006/01/02 15:04:05.0000"), e.Severity, msg)
+	if e.Error.Err != nil {
+		l.t.Logf("\t\terr=%s\n", e.Error.Err.Error())
+	}
+	if len(fields) > 0 {
+		l.t.Logf("\t\t%s\n", strings.Join(fields, "\t"))
+	}
+	if e.Error.Stacktrace != "" {
+		l.t.Logf("\nStacktrace:\n%s\n", e.Error.Stacktrace)
+	}
+}
+
+func (l Logger) Flush() error {
+	return nil
+}
